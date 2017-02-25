2e0cdd9563650c7a8407a44b7920d219a30f2265
jeff60907 <jeff60907@gmail.com>
2017-02-25 20:56:54 +0800

    improve entry lastName struct

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -5,7 +5,7 @@
 
 /* TODO: After modifying the original version, uncomment the following
  * line to set OPT properly */
-// #define OPT 1
+#define OPT 1
 typedef struct __PHONE_BOOK_ENTRY {
     char lastName[MAX_LAST_NAME_SIZE];
     char firstName[16];
@@ -18,8 +18,13 @@ typedef struct __PHONE_BOOK_ENTRY {
     char state[2];
     char zip[5];
     struct __PHONE_BOOK_ENTRY *pNext;
-} entry;
+} entry_detail;
 
+typedef struct _LASTNAME_ENTRY {
+    char lastName[MAX_LAST_NAME_SIZE];
+    entry_detail *detail;
+    struct _LASTNAME_ENTRY *pNext;
+} entry;
 entry *findName(char lastName[], entry *pHead);
 entry *append(char lastName[], entry *e);
 
